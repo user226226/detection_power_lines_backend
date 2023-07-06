4e2a1a2a626d4ca90a1cf60fccdc687efcf817e5
Anton Zakharov <anton.zakharov01@gmail.com>
2023-07-06 10:58:55 +0300

    Fix styles

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 import codecs
 
 app = FastAPI()
@@ -11,3 +11,7 @@ def read_root():
     f=codecs.open("./pages/main.html", 'r')
     content = f.read()
     return HTMLResponse(content=content)
+
+@app.get("/styles/main.css")
+async def get_auth_style():
+    return FileResponse('styles/main.css')
