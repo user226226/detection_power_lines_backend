78651cb3b61b191c735c6569e718f8cdd3109f22
Anton Zakharov <anton.zakharov01@gmail.com>
2023-07-07 10:23:23 +0300

    Code 8

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, FileResponse
+from model import get_predict_image
+
 import codecs
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    f=codecs.open("./pages/main.html", 'r')
+    f=codecs.open("./pages/main.html", 'r', encoding="UTF-8")
     content = f.read()
     return HTMLResponse(content=content)
 
@@ -18,15 +20,14 @@ async def get_auth_style():
 async def upload(file: UploadFile = File(...)):
     try:
         contents = file.file.read()
-        with open("images/result_image.jpg", "wb") as f:
-            f.write(contents)
         with open("images/select_image.jpg", "wb") as f:
             f.write(contents)
+        
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()    
-    return "ok"
+    return get_predict_image("images/select_image.jpg")
 
 @app.get("/images/select_image.jpg")
 async def get_result_image():
--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="MvcIZgKbt2jU2rC8bRXy")
-project = rf.workspace().project("detection-power-lines")
-model = project.version(1).model
 
-# infer on a local image
-print(model.predict("images/image.jpg", confidence=40, overlap=30).json())
+def get_predict_image(path: str):
+    rf = Roboflow(api_key="MvcIZgKbt2jU2rC8bRXy")
+    project = rf.workspace().project("detection-power-lines")
+    model = project.version(1).model
 
-# visualize your prediction
-model.predict("images/image.jpg", confidence=40, overlap=30).save("images/prediction.jpg")
+    result_model = model.predict(path, confidence=40, overlap=30)
+    result_model.save("images/result_image.jpg")
+    return result_model.json()
