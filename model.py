ef4a5011bf20d0b87f5d6087f0cc742364385340
Anton Zakharov <anton.zakharov01@gmail.com>
2023-07-07 13:20:45 +0300

    Add model

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 from roboflow import Roboflow
 
-def get_predict_image(path: str):
-    rf = Roboflow(api_key="MvcIZgKbt2jU2rC8bRXy")
-    project = rf.workspace().project("detection-power-lines")
-    model = project.version(1).model
+rf = Roboflow(api_key="MvcIZgKbt2jU2rC8bRXy")
+project = rf.workspace().project("detection-power-lines")
+model = project.version(1).model
 
+def get_predict_image(path: str):
     result_model = model.predict(path, confidence=40, overlap=30)
     result_model.save("images/result_image.jpg")
     return result_model.json()
